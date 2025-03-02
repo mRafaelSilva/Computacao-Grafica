de34a0128492e5bf1654af771ec1618396000d6e
MariaCletoR <maria.cleto.rocha@gmail.com>
2025-03-02 00:08:33 +0000

    parsexml

--- Generator/engine.cpp
+++ Generator/engine.cpp
@@ -3,8 +3,74 @@
 #include <fstream>
 #include <sstream>
 #include <iostream>
+#include "tinyxml2.h"
+
+using namespace tinyxml2;
 
 std::vector<float> vertices;
+std::vector<std::string> modelFiles;
+
+struct Camera {
+    float posX, posY, posZ;
+    float lookAtX, lookAtY, lookAtZ;
+    float upX, upY, upZ;
+    float fov, near, far;
+}camera;
+
+int width;
+int height;
+
+void parsexml(const char* file) {
+    XMLDocument doc;
+    if (doc.LoadFile(file) != XML_SUCCESS) {
+        std::cerr << "Error loading XML file!" << std::endl;
+        return;
+    }
+    XMLElement* first = doc.FirstChildElement("world");
+    XMLElement * windowElem = root->FirstChildElement("window");
+    if (windowElem) {
+        windowElem->QueryIntAttribute("width", &width);
+        windowElem->QueryIntAttribute("height", &height);
+    }
+    XMLElement* camElem = root->FirstChildElement("camera");
+    if (camElem) {
+        XMLElement* pos = camElem->FirstChildElement("position");
+        if (pos) pos->QueryFloatAttribute("x", &camera.posX);
+        if (pos) pos->QueryFloatAttribute("y", &camera.posY);
+        if (pos) pos->QueryFloatAttribute("z", &camera.posZ);
+
+        XMLElement* lookAt = camElem->FirstChildElement("lookAt");
+        if (lookAt) lookAt->QueryFloatAttribute("x", &camera.lookAtX);
+        if (lookAt) lookAt->QueryFloatAttribute("y", &camera.lookAtY);
+        if (lookAt) lookAt->QueryFloatAttribute("z", &camera.lookAtZ);
+
+        XMLElement* up = camElem->FirstChildElement("up");
+        if (up) up->QueryFloatAttribute("x", &camera.upX);
+        if (up) up->QueryFloatAttribute("y", &camera.upY);
+        if (up) up->QueryFloatAttribute("z", &camera.upZ);
+
+        XMLElement* proj = camElem->FirstChildElement("projection");
+        if (proj) proj->QueryFloatAttribute("fov", &camera.fov);
+        if (proj) proj->QueryFloatAttribute("near", &camera.near);
+        if (proj) proj->QueryFloatAttribute("far", &camera.far);
+    }
+
+    XMLElement* groupElem = root->FirstChildElement("group");
+    if (groupElem) {
+        XMLElement* modelsElem = groupElem->FirstChildElement("models");
+        if (modelsElem) {
+            XMLElement* model = modelsElem->FirstChildElement("model");
+            while (model) {
+                const char* file = model->Attribute("file");
+                if (file) {
+                    modelFiles.push_back(file);
+                    loadModel(file);
+                }
+                model = model->NextSiblingElement("model");
+            }
+        }
+    }
+}
 
 void loadModel(const char* filename) {
     std::ifstream file(filename);
@@ -63,7 +129,7 @@ void changeSize(int w, int h) {
     glLoadIdentity();
     
     glViewport(0, 0, w, h);
-    gluPerspective(45.0f, ratio, 1.0f, 1000.0f);
+    gluPerspective(camera.fov, ratio, camera.near, camera.far);
 
     glMatrixMode(GL_MODELVIEW);
 }
@@ -71,9 +137,9 @@ void changeSize(int w, int h) {
 void display() {
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
     glLoadIdentity();
-    gluLookAt(2.5f, 2.5f, 6.0f,
-              0.0f, 0.0f, 0.0f, 
-              0.0f, 1.0f, 0.0f);
+    gluLookAt(camera.posX, camera.posY, camera.posZ,
+              camera.lookAtX, camera.lookAtY, camera.lookAtZ, 
+              camera.upX, camera.upY, camera.upZ);
 
     // Desenha os eixos ANTES do cubo
     glDisable(GL_DEPTH_TEST); // Para garantir que os eixos são sempre visíveis
@@ -97,11 +163,9 @@ void display() {
 
 
 int main(int argc, char** argv) {
-    loadModel("C:/Users/mrafa/OneDrive/Ambiente de Trabalho/Uni/CG/projeto/Generator/build/Release/cone.3d");
-
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGBA);
-    glutInitWindowSize(800, 800);
+    glutInitWindowSize(width, height);
     glutInitWindowPosition(100, 100);
     glutCreateWindow("3D Engine");
 
